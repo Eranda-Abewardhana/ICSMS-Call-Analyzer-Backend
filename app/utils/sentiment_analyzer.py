82193584c68a83fb4557c428377d54ec0983500f
Hirusha Fernando <heshanhfernando@gmail.com>
2024-03-13 08:34:55 +0530

    Code refactored

--- app/utils/sentiment_analyzer.py
+++ app/utils/sentiment_analyzer.py
@@ -7,17 +7,17 @@ os.environ['GOOGLE_API_KEY'] = "AIzaSyAkwOJZV94Wuq-96EFe17HP-O5VRk7sKyc"
 
 
 class SentimentAnalyzer:
-    __model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7)
-    __output_parser = StrOutputParser()
-    __template = ("Use this telephone call transcription {transcript} to detect the customer who is calling and the call "
-                "operator who is handling the call. then identify the emotion of the customer and categorize the "
-                "emotion into one of these categories. {categories} only output should be the category")
+    def __init__(self):
+        self.__model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7)
+        self.__output_parser = StrOutputParser()
+        self.__template = ("Use this telephone call transcription {transcript} to detect the customer who is calling and the call "
+                    "operator who is handling the call. then identify the emotion of the customer and categorize the "
+                    "emotion into one of these categories. {categories} only output should be the category")
 
-    __prompt_template = PromptTemplate(template=__template, input_variables=["transcript", "categories"])
+        self.__prompt_template = PromptTemplate(template=self.__template, input_variables=["transcript", "categories"])
 
-    @classmethod
-    def analyze(cls, call_transcription: str, sentiment_categories: list[str]) -> str:
-        chain = cls.__prompt_template | cls.__model | cls.__output_parser
+    def analyze(self, call_transcription: str, sentiment_categories: list[str]) -> str:
+        chain = self.__prompt_template | self.__model | self.__output_parser
         sentiment = chain.invoke({"transcript": call_transcription, "categories": sentiment_categories})
         return sentiment
 
