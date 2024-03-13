4bd442b4769b29ef3490c209fbc2cfe3f246cfbf
Hirusha Fernando <heshanhfernando@gmail.com>
2024-03-13 18:04:21 +0530

    Code Changes

--- app/database/db.py
+++ app/database/db.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from pymongo import MongoClient
+import motor.motor_asyncio
 from pymongo.errors import ServerSelectionTimeoutError
 
 from app.config.constants import TextMessages
@@ -10,7 +10,7 @@ class DatabaseConnector:
     def __init__(self, collection_name: str):
         self.__connection_string = "mongodb+srv://heshanhfernando:MVCld4tFIE1zeaMQ@hellocluster.9skju6b.mongodb.net/"
         self.__database_name = "call_recordings"
-        self.__client = MongoClient(self.__connection_string)
+        self.__client = motor.motor_asyncio.AsyncIOMotorClient(self.__connection_string)
         try:
             self.__client.server_info()
             self.__database = self.__client.get_database(self.__database_name)
