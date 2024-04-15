694a65145188d92120392d77e84d39d17b71508f
Hirusha Fernando <heshanhfernando@gmail.com>
2024-04-15 19:42:10 +0530

    sentiment categories changes

--- app/config/config.py
+++ app/config/config.py
@@ -4,5 +4,5 @@ from pathlib import Path
 class Configurations:
     UPLOAD_FOLDER = Path(".\data").resolve()
     SAVED_FOLDER = Path(".\mp3").resolve()
-    sentiment_categories = ["Sad", "Anger", "Toxic", "Happy"]
+    sentiment_categories = ["Positive", "Negative", "Neutral"]
 
