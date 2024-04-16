6d1ce0839bd293f4463aeb404275b7b19864e5bd
Hirusha Fernando <heshanhfernando@gmail.com>
2024-04-16 09:08:18 +0530

    Connection string changed

--- app/database/db.py
+++ app/database/db.py
@@ -12,7 +12,7 @@ from app.models.action_result import ActionResult
 
 class DatabaseConnector:
     def __init__(self, collection_name: str):
-        self.__connection_string = "mongodb+srv://rmb18132:4wgWW4X3q4RLLR2@cluster0.mzrfjau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        self.__connection_string = "mongodb+srv://erandaabewardhana:19765320@cluster0.7coezqv.mongodb.net/users?retryWrites=true&w=majority"
         self.__database_name = "call_recordings"
         self.__client = motor.motor_asyncio.AsyncIOMotorClient(self.__connection_string)
         try:
