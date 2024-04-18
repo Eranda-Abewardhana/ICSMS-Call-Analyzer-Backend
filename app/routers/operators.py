686c36e0b044e8bcc6d426e2042236a335679cf3
Hirusha Fernando <heshanhfernando@gmail.com>
2024-04-18 15:55:45 +0530

    operators delete, update ops

--- app/routers/operators.py
+++ app/routers/operators.py
@@ -4,6 +4,8 @@ from app.database.db import DatabaseConnector
 from app.models.action_result import ActionResult
 from app.models.call_operator import CallOperator
 from app.database.aggregation import get_next_operator_id_pipeline
+from app.models.operator_dto import CallOperatorDTO
+
 operator_router = APIRouter()
 
 db = DatabaseConnector("operators")
@@ -12,6 +14,11 @@ db = DatabaseConnector("operators")
 @operator_router.get('/get-all-operators', response_model=ActionResult)
 async def get_all_operators():
     action_result = await db.get_all_entities()
+    operators = []
+    for operator in action_result.data:
+        operator['id'] = operator['_id']['$oid']
+        operators.append(operator)
+    action_result.data = operators
     return action_result
 
 
@@ -34,8 +41,9 @@ async def add_operator(operator: CallOperator):
     return action_result
 
 
-@operator_router.put('/update-operator/{operator_id}', response_model=ActionResult)
-async def update_operator(operator: CallOperator):
+@operator_router.put('/update-operator', response_model=ActionResult)
+async def update_operator(operatorDTO: CallOperatorDTO):
+    operator = CallOperator(name=operatorDTO.name, operator_id=operatorDTO.operator_id, _id=operatorDTO.id)
     action_result = await db.update_entity(operator)
     return action_result
 
