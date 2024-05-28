33b937103775421fdbbcbd7a687b9e98f33dfc49
RanushMithila <ranushmithila45@gmail.com>
2024-05-28 20:25:07 +0530

    call settings

--- app/models/notification_settings.py
+++ app/models/notification_settings.py
@@ -3,19 +3,28 @@ from pydantic import BaseModel, Field, ConfigDict
 from typing_extensions import Annotated
 from pydantic.functional_validators import BeforeValidator
 
+
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class NotificatioSettings(BaseModel):
-    id: Optional[str] = Field(alias="_id", default=None)
+    id: Optional[str] = Field(alias="_id", default='')
     is_email_alerts_enabled: bool = Field(...)
     user_id: str = Field(...)
     is_lower_threshold_enabled: bool = Field(...)
     is_upper_threshold_enabled: bool = Field(...)
     sentiment_lower_threshold: float = Field(...)
     sentiment_upper_threshold: float = Field(...)
-    call_directory: str = Field(...)
+    call_directory: Optional[str] = Field(None)
     alert_keywords: list[str] = Field(...)
-    alert_email_receptions: str = Field(...)
+    alert_email_receptions: list[str] = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+   )
+    
+class NotificatioDirSettings(BaseModel):
+    id: str = Field(alias="_id", default='')
+    dir: str = Field(...)
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True
--- app/routers/settings.py
+++ app/routers/settings.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException, UploadFile, File
 
 from app.database.db import DatabaseConnector
 from app.models.action_result import ActionResult
-from app.models.notification_settings import NotificatioSettings
+from app.models.notification_settings import NotificatioDirSettings, NotificatioSettings
 
 settings_router = APIRouter()
 
@@ -17,12 +17,14 @@ async def get_notification_settings(userID: str):
     action_result.data = [x for x in action_result.data if x["user_id"] == userID]
     return action_result
 
-@settings_router.post("/add-notification-settings", response_model=ActionResult)
-async def add_notification_settings(settings: NotificatioSettings):
-    action_result = await db.add_entity(settings)
-    return action_result
-
 @settings_router.post("/update-notification-settings", response_model=ActionResult)
 async def update_notification_settings(settings: NotificatioSettings):
+    print(settings.dict())
+    action_result = await db.update_entity(settings)
+    return action_result
+
+@settings_router.post("/update-dir-settings", response_model=ActionResult)
+async def update_notification_settings(settings: NotificatioDirSettings):
+    print(settings.dict())
     action_result = await db.update_entity(settings)
     return action_result
