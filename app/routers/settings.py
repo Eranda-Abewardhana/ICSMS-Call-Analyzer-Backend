a5c5604a799999f258b74762116e06905b6d5af8
RanushMithila <ranushmithila45@gmail.com>
2024-06-07 18:46:03 +0530

    call settings updated

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -1,35 +1,31 @@
-import os
-import shutil
-from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi import APIRouter
 
 from app.database.db import DatabaseConnector
 from app.models.action_result import ActionResult
-from app.models.notification_settings import NotificatioDirSettings, NotificatioSettings
+from app.models.notification_settings import CallSettings
+from app.models.settings_dto import SettingsDTO
 
 settings_router = APIRouter()
 
 db = DatabaseConnector("settings")
-@settings_router.get("/get-notification-settings/{userID}", response_model=ActionResult)
-async def get_notification_settings(userID: str):
+
+
+@settings_router.get("/get-notification-settings", response_model=ActionResult)
+async def get_notification_settings():
     action_result = await db.get_all_entities()
-    # filter the result to get the user's settings
-    action_result.data = [x for x in action_result.data if x["user_id"] == userID]
+    settings_id = action_result.data[0]["_id"]["$oid"]
+    action_result.data[0]["id"] = settings_id
+    action_result.data = action_result.data[0]
+    del action_result.data["_id"]
     return action_result
+
 
 @settings_router.post("/update-notification-settings", response_model=ActionResult)
-async def update_notification_settings(settings: NotificatioSettings):
-    querry_id = await db.find_entity({"user_id": settings.user_id})
-    settings.id = querry_id.data["_id"]['$oid']
-    print(settings.id)
-    action_result = await db.update_entity(settings)
-    return action_result
-
-@settings_router.post("/update-dir-settings", response_model=ActionResult)
-async def update_dir_settings(settings: NotificatioDirSettings):
-    print(settings.dict())
-    # print(settings)
-    querry_id = await db.find_entity({"user_id": settings.user_id})
-    settings.id = querry_id.data["_id"]['$oid']
-    print(settings.id)
-    action_result = await db.update_entity(settings)
+async def update_notification_settings(settings: SettingsDTO):
+    settings_dict = settings.dict()
+    settings_id = settings_dict["id"]
+    settings_dict["_id"] = settings_id
+    del settings_dict["id"]
+    call_settings = CallSettings(**settings_dict)
+    action_result = await db.update_entity(call_settings)
     return action_result
